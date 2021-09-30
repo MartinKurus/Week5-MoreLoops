89edccd5d0dd394b2a76b61adab06b7368dbc864
Opilane TTHK <opilane@tthk.ee>
2021-09-30 11:28:09 +0300

    first commit

---
+++ GuessingGameThreeTries/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace GuessingGameThreeTries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int cpuRandom = rnd.Next(1, 11);
+
+            int i = 0;
+            while(i < 3)
+            {
+                Console.WriteLine("Vali number 1-10:");
+                int userNumber = Convert.ToInt32(Console.ReadLine());
+
+                if (cpuRandom == userNumber)
+                {
+                    Console.WriteLine("Palju õnne! Oled võitnud!");
+                    i = 3;
+                }
+                else
+                {
+                    i = i + 1;
+                    if(i < 3)
+                    {
+                        Console.WriteLine($"Proovi uuesti. {3 - i} katsed on jäänud.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Arvuti on võitnud.");
+                    }
+                    
+                }
+            }
+            Console.WriteLine("Kena päeva!");
+        }
+    }
+}
---
+++ MoreLoops/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MoreLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm genereerib juhusliku numbrit 1 kuni 10;
+            //kasutaja peab seda numbrit ära arvama;
+            //kui kasutaja suutis seda numbrit ära arvata, siis ta on mängu võitnud;
+            //katsete arv on piirmatu;
+            //programm genereerib juhusliku numbrit ühte korda.
+
+            Random rnd = new Random();
+            int cpuRandom = rnd.Next(1, 11);
+            bool loopActive = true;
+            while (loopActive)
+            {
+                Console.WriteLine("Vali number 1-10:");
+                int userNumber = Convert.ToInt32(Console.ReadLine());
+
+                if (cpuRandom == userNumber)
+                {
+                    Console.WriteLine("Palju õnne! Oled võitnud!");
+                    loopActive = false;
+                }
+                else
+                {
+                    Console.WriteLine("Proovi uuesti.");
+                }
+            }
+
+            Console.WriteLine("Kena päeva!");
+        }
+    }
+}
